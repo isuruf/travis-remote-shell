dfcfd75afab97aebb3755ac1c6c1be2f07f4b421
Isuru Fernando <isuru.11@cse.mrt.ac.lk>
2016-08-27 12:32:51 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+if [ "$(uname)" == "Darwin" ]; then
+    wget -O ngrok.zip https://dl.ngrok.com/ngrok_2.0.19_linux_amd64.zip
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    wget -O ngrok.zip https://dl.ngrok.com/ngrok_2.0.19_darwin_amd64.zip
+fi
 
-wget -O ngrok.zip https://dl.ngrok.com/ngrok_2.0.19_linux_amd64.zip
 unzip ngrok.zip
 {
-    nc -l -v -p 8888 -e /bin/bash
+    nc -l -v -c /bin/bash 8888
     killall -SIGINT ngrok && echo "ngrok terminated"
 } &
 {
